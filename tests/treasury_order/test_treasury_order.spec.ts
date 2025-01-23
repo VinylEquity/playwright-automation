f20499e74a9057d2a94bd6102a986c10b090971d
dilip-vinyl <dilip.kumar@vinylequity.com>
2025-01-23 10:57:20 +0530

    2628 added separate test for regression testing

--- tests/treasury_order/test_treasury_order.spec.ts
+++ tests/treasury_order/test_treasury_order.spec.ts
@@ -4,7 +4,7 @@ import { randomInt } from 'crypto';
 import { timeHelper } from '../../support/time.helper';
 import { mailerMethods } from '../../support/mailer.methods';
 
-test.describe("Vinyl Treasury Order", {tag: '@dev_sanity'}, async () => {
+test.describe("Vinyl Treasury Order", async () => {
   let VinylPages: vinylPages;
   let quantity: number;
 
@@ -23,7 +23,48 @@ test.describe("Vinyl Treasury Order", {tag: '@dev_sanity'}, async () => {
     await page.close();
   });
 
-  test('Create New Treasury Order and automatic release of Treasury Order', async ({ page, request }) => {
+  test('Create New Treasury Order and release of Treasury Order via API',{tag: '@dev_sanity'}, async ({ page, request }) => {
+    const name = 'Test ' + randomInt(0,999);
+    const description = 'Description Test ' + randomInt(0,999);
+    var automatic_release, url, subject, to_id;
+    await VinylPages.SignInPage.login(`${process.env.TA_USER}`);
+    await VinylPages.PhoneVerificationPage.enter_valid_otp();
+    await VinylPages.DashboardPage.go_to_treasury_order_page();
+    await page.waitForURL(`${process.env.HOST}issuers/treasury-orders`);
+    await VinylPages.TreasuryOrderPage.select_issuer(process.env.ISSUER);
+    await VinylPages.TreasuryOrderPage.create_treasury_order.click();
+    await page.waitForURL(`${process.env.HOST}issuers/treasury-orders/create?type=ISSUANCE`);
+    automatic_release = await VinylPages.TreasuryOrderPage.enter_TO_details(name, process.env.ISSUE, 'IPO', description, 'Email');
+    await VinylPages.TreasuryOrderPage.add_existing_automation_ro_recipient(process.env.RO_USER);
+    await VinylPages.TreasuryOrderPage.validate_recipent_added('automation', process.env.RO_USER, process.env.RO_TIN);
+    await VinylPages.TreasuryOrderPage.enter_quantity_and_price(1, 1);
+    await VinylPages.TreasuryOrderPage.submit_TO();
+    await VinylPages.TreasuryOrderPage.is_TO_submitted();
+    await VinylPages.TreasuryOrderPage.validate_TO_document(name);
+    await VinylPages.TreasuryOrderPage.validate_TO_details(name, description, 'IPO', process.env.ISSUER, process.env.ISSUE, 1, 'Email');
+    url = await page.url();
+    to_id = url.replace(process.env.HOST + 'issuers/treasury-orders/', '').replace('#', '');
+    const response = await request.post(`${process.env.LEDGER_SERVICE_API_BASE_URL}/treasury-orders/${to_id}/release`,{
+      headers:{
+        'X-API-KEY': `${process.env.API_TOKEN}`,
+      }
+    });
+    expect(response.ok()).toBeTruthy();
+    subject = process.env.ISSUER + " has issued " + process.env.ISSUE + " to AUTOMATION";
+    await page.reload();
+    await page.waitForURL(url);
+    await mailerMethods.get_TO_mail(subject, `${process.env.RO_USER}`);
+    await VinylPages.TreasuryOrderPage.is_TO_release_completed();
+    await VinylPages.DashboardPage.logout();
+
+    await VinylPages.SignInPage.login(`${process.env.RO_USER}`);
+    await VinylPages.PhoneVerificationPage.enter_valid_otp();
+    await page.waitForURL(`${process.env.HOST}portfolio`);
+    await expect(await VinylPages.PortfolioPage.get_issuer_quantity(process.env.ISSUE, process.env.ISSUER)).toBe(quantity + 1);
+    await VinylPages.PortfolioPage.logout();
+  });
+
+  test('Create New Treasury Order and automatic release of Treasury Order',{tag: '@regression'}, async ({ page, request }) => {
     const name = 'Test ' + randomInt(0,999);
     const description = 'Description Test ' + randomInt(0,999);
     var automatic_release, url, automatic_release_time, subject, wait_time, to_id;
@@ -57,7 +98,7 @@ test.describe("Vinyl Treasury Order", {tag: '@dev_sanity'}, async () => {
       });
       expect(response.ok()).toBeTruthy();
     }
-    subject = process.env.ISSUER + " has issued " + process.env.ISSUE + " to automation";
+    subject = process.env.ISSUER + " has issued " + process.env.ISSUE + " to AUTOMATION";
     await page.reload();
     await page.waitForURL(url);
     await mailerMethods.get_TO_mail(subject, `${process.env.RO_USER}`);
