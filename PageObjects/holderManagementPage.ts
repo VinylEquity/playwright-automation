f231bc4385407c291a38288c67068f3ec161c085
dilip-vinyl <dilip.kumar@vinylequity.com>
2025-01-29 17:34:07 +0530

    added tests for holder and position stops

--- PageObjects/holderManagementPage.ts
+++ PageObjects/holderManagementPage.ts
@@ -1,5 +1,6 @@
 import { expect, type Locator, type Page } from '@playwright/test';
 
+
 export class holderManagementPage{
     readonly page: Page
     readonly search_holder: Locator;
@@ -10,6 +11,23 @@ export class holderManagementPage{
     readonly gen_drs_stmt: Locator;
     readonly date_from: Locator;
     readonly date_to: Locator;
+    readonly holder_stop_btn: Locator;
+    readonly position_stop_btn: Locator;
+    readonly holder_stop_popup: Locator;
+    readonly position_stop_popup: Locator;
+    readonly holder_stop_reason: Locator;
+    readonly holder_stop_additional_info: Locator;
+    readonly holder_stop_document: Locator;
+    readonly holder_stop_doc_uploaded: Locator;
+    readonly holder_stop_save: Locator;
+    readonly alert: Locator;
+    readonly manage_holder_stop_btn: Locator;
+    readonly manage_position_stop_btn: Locator;
+    readonly holder_stop_reason_info: Locator;
+    readonly manager_holder_stop_popup: Locator;
+    readonly manage_holder_stops_table: Locator;
+    readonly manage_position_stops_table: Locator;
+    readonly holders_table: Locator;
 
     constructor(page: Page){
         this.page = page;
@@ -21,6 +39,23 @@ export class holderManagementPage{
         this.gen_drs_stmt = page.getByLabel('Generate DRS Statement').locator('div').filter({ hasText: /^Generate DRS Statement$/ });
         this.date_from = page.getByLabel('Date From');
         this.date_to = page.getByLabel('Date To');
+        this.holder_stop_btn = page.getByRole('button', { name: 'Add Stop' }).first();
+        this.position_stop_btn = page.getByRole('button', { name: 'Add Stop' }).nth(1);
+        this.holder_stop_popup =  page.getByLabel('Add Holder Stop');
+        this.position_stop_popup =  page.getByLabel('Add Position Stop');
+        this.holder_stop_reason = page.getByLabel('Reason for Stop');
+        this.holder_stop_additional_info = page.locator('input[name="additionalInfo"]');
+        this.holder_stop_document = page.getByRole('button', { name: 'Upload supporting document' });
+        this.holder_stop_save = page.getByRole('button', { name: 'Save' });
+        this.holder_stop_doc_uploaded = page.getByText('Selected 1 file');
+        this.alert = page.locator('[id="__next"]').getByRole('alert');
+        this.manage_holder_stop_btn = page.getByRole('button', { name: 'Manage Stops' });
+        this.manage_position_stop_btn = page.getByRole('button', { name: 'Manage Stop' }).nth(1)
+        this.holder_stop_reason_info = page.locator('div').filter({ hasText: /^Other$/ })
+        this.manager_holder_stop_popup = page.getByLabel('Manage Holder Stop');
+        this.manage_holder_stops_table = page.getByLabel('Manage Holder Stop').locator('table.MuiTable-root tbody');
+        this.manage_position_stops_table = page.getByLabel('Manage Positions Stop').locator('table.MuiTable-root tbody');
+        this.holders_table = page.locator('table.MuiTable-root tbody').nth(0);
     }
 
     async search_for_holder(holder){
@@ -29,7 +64,7 @@ export class holderManagementPage{
         await this.page.getByRole('option', { name: holder }).click();
     }
    
-    async validate_holder_details(name, email, tin, type){
+    async validate_holder_details_TA(name, email, tin, type){
         await expect(this.holder_details).toBeVisible();
         await expect(this.page.getByText(`Legal Name: ${name}`)).toBeVisible();
         await expect(this.page.getByText(`Email Address: ${email}`)).toBeVisible();
@@ -44,19 +79,91 @@ export class holderManagementPage{
         await expect(this.page.getByText(`Holder Type: ${type}`)).toBeVisible();
     }
 
-    async generate_drs_statement_for_RO(name, issue){
+    async add_holder_stop(){
+        await expect(this.holder_stop_btn).toBeVisible();
+        await expect(this.holder_stop_btn).toBeEnabled();
+        await this.holder_stop_btn.click();
+        await expect(this.holder_stop_popup).toBeVisible();
+        await this.holder_stop_reason.click();
+        await this.page.getByRole('option', { name: 'Other' }).click();
+        await this.holder_stop_additional_info.fill('Test');
+        await this.holder_stop_document.setInputFiles('test_data/presigned_document.pdf');
+        await this.page.waitForTimeout(5000); // waiting time to upload the document
+        await this.holder_stop_doc_uploaded.waitFor();
+        await this.holder_stop_save.click()
+    }
+    
+    async add_position_stop(){
+        await this.holders_table.scrollIntoViewIfNeeded();
+        await expect(this.position_stop_btn).toBeDisabled();
+        await this.holders_table.locator('tr').nth(0).locator('td').nth(0).click();
+        await this.holders_table.locator('tr').nth(1).locator('td').nth(1).click();
+        await expect(this.position_stop_btn).toBeEnabled();
+        await this.position_stop_btn.click();
+        await expect(this.position_stop_popup).toBeVisible();
+        await this.holder_stop_reason.click();
+        await this.page.getByRole('option', { name: 'Other' }).click();
+        await this.holder_stop_additional_info.fill('Test');
+        await this.holder_stop_save.click()
+    }
+
+    async validate_holder_stop_added(){
+        await this.alert.waitFor();
+        await expect(this.alert).toHaveText("Added holder stop successfully");
+        await expect(this.manage_holder_stop_btn).toBeVisible();
+        await expect(this.manage_holder_stop_btn).toBeEnabled();
+        await expect(this.holder_stop_reason_info).toBeVisible();
+        await this.manage_holder_stop_btn.click();
+        await expect(this.manage_holder_stops_table.locator('tr').nth(0).locator('td').nth(2)).toHaveText('Other');
+        await expect(this.manage_holder_stops_table.locator('tr').nth(0).locator('td').nth(3)).toHaveText(new Date().toLocaleDateString("fr-CA", {year:"numeric", month: "2-digit", day:"2-digit"}));
+        await expect(this.manage_holder_stops_table.locator('tr').nth(0).locator('td').nth(4)).toHaveText('');
+        await expect(this.manage_holder_stops_table.locator('tr').nth(0).locator('td').nth(5)).toHaveText('Test');
+    }
+
+    async validate_position_stop_added(){
+        await this.alert.waitFor();
+        await expect(this.alert).toHaveText("Added position stop successfully");
+        await this.holders_table.scrollIntoViewIfNeeded();
+        await expect(this.position_stop_btn).toBeDisabled();
+        await expect(this.manage_position_stop_btn).toBeDisabled();
+        await this.holders_table.locator('tr').nth(1).locator('td').nth(1).click();
+        await expect(this.manage_position_stop_btn).toBeEnabled();
+        await this.manage_position_stop_btn.click();
+        await expect(this.manage_position_stops_table.locator('tr').nth(0).locator('td').nth(2)).toHaveText('Other');
+        await expect(this.manage_position_stops_table.locator('tr').nth(0).locator('td').nth(3)).toHaveText(new Date().toLocaleDateString("fr-CA", {year:"numeric", month: "2-digit", day:"2-digit"}));
+        await expect(this.manage_position_stops_table.locator('tr').nth(0).locator('td').nth(4)).toHaveText('');
+        await expect(this.manage_position_stops_table.locator('tr').nth(0).locator('td').nth(5)).toHaveText('Test');
+    }
+
+    async remove_stop(){
+        this.page.getByRole('row', { name: 'Expand Other' }).getByRole('button').nth(1).click();
+    }
+
+
+    async validate_holder_stop_removed(){
+        await this.alert.waitFor();
+        await expect(this.alert).toHaveText("Removed holder stop successfully");
+        await expect(this.manage_holder_stop_btn).toBeVisible();
+        await expect(this.manage_holder_stop_btn).toBeDisabled();
+    }
+
+    async validate_position_stop_removed(){
+        await this.alert.waitFor();
+        await expect(this.alert).toHaveText("Removed position stop successfully");
+        await this.holders_table.locator('tr').nth(1).locator('td').nth(1).click();
+        await expect(this.manage_position_stop_btn).toBeDisabled();
+    }
+
+    async generate_drs_statement_for_RO(name, issue, from_date, to_date){
         await this.generate_drs_statement.click();
         await expect(this.generate_drs_statement_popup).toBeVisible();
         await expect(this.page.getByLabel('Generate DRS Statement').getByText(name)).toBeVisible();
         await this.select_issue.click();
         await this.page.getByRole('option', { name: issue }).click();
-        await this.date_from.fill('2025-01-24');
-        await this.date_to.fill('2025-01-28');
+        await this.date_from.fill(from_date);
+        await this.date_to.fill(to_date);
         await this.gen_drs_stmt.click();
         await this.page.getByRole('button', { name: 'Print' }).click();
-        await this.page.pdf(path='test.pdf');
-        // await this.page.locator('div:nth-child(4) > div > .MuiBox-root > div > div').first().waitFor();
-        // await this.page.locator('div:nth-child(4) > div > .MuiBox-root > div > div').first().click();
-
+        return await this.page.locator('iframe').getAttribute("src");
     }
 }
--- tests/holder_management/test_holder_management.spec.ts
+++ tests/holder_management/test_holder_management.spec.ts
@@ -1,17 +1,20 @@
 import { test, expect } from '@playwright/test';
 import { vinylPages } from '../../PageObjects/vinylPages'
+import { fileMethods } from '../../support/fileMethods';
 
-test.describe("Vinyl Holder Management Tests", {tag: ['@search']}, async () => {
+test.describe("Vinyl Holder Management Tests", {tag: ['@holder_management']}, async () => {
   let VinylPages: vinylPages;
+  let FileMethods: fileMethods;
 
   test.beforeEach(async ({ page }) => {
     VinylPages = new vinylPages(page);
+    FileMethods = new fileMethods(page);
     await page.goto(`${process.env.HOST}`); // open the app
     await expect(page.getByText('Enter Your Email To Sign In')).toBeVisible() // make sure app is in login page 
   });
   
   test.afterEach(async ({page}) => {
-    // await page.close();
+    await page.close();
   });
 
   test('Search for RO user via TA', async ({ page }) => {
@@ -23,7 +26,7 @@ test.describe("Vinyl Holder Management Tests", {tag: ['@search']}, async () => {
     for(var i = 0; i < 4; i++){
       await expect(VinylPages.HolderManagementPage.holder_details).not.toBeVisible();
       await VinylPages.HolderManagementPage.search_for_holder(search_details[i]); 
-      await VinylPages.HolderManagementPage.validate_holder_details(process.env.RO2_USERNAME, process.env.RO_USER1, process.env.RO2_TIN, 'Individual');
+      await VinylPages.HolderManagementPage.validate_holder_details_TA(process.env.RO2_USERNAME, process.env.RO_USER1, process.env.RO2_TIN, 'Individual');
       await page.reload();
     }
     await VinylPages.DashboardPage.logout();
@@ -44,6 +47,27 @@ test.describe("Vinyl Holder Management Tests", {tag: ['@search']}, async () => {
     await VinylPages.DashboardPage.logout();
   });
 
+  test('Add and remove holder and position stop to a RO via TA', async ({ page }) => {
+    await VinylPages.SignInPage.login(`${process.env.TA_USER1}`);
+    await VinylPages.PhoneVerificationPage.enter_valid_otp();
+    await page.waitForURL(`${process.env.HOST}dashboard`);
+    await VinylPages.DashboardPage.go_to_holder_management();
+    await expect(VinylPages.HolderManagementPage.holder_details).not.toBeVisible();
+    await VinylPages.HolderManagementPage.search_for_holder(process.env.RO_USER1); 
+    await VinylPages.HolderManagementPage.validate_holder_details_TA(process.env.RO2_USERNAME, process.env.RO_USER1, process.env.RO2_TIN, 'Individual');
+    //  Test Holder Stop
+    await VinylPages.HolderManagementPage.add_holder_stop();
+    await VinylPages.HolderManagementPage.validate_holder_stop_added();
+    await VinylPages.HolderManagementPage.remove_stop();
+    await VinylPages.HolderManagementPage.validate_holder_stop_removed();
+    // Test Position Stop
+    await VinylPages.HolderManagementPage.add_position_stop();
+    await VinylPages.HolderManagementPage.validate_position_stop_added();
+    await VinylPages.HolderManagementPage.remove_stop();
+    await VinylPages.HolderManagementPage.validate_position_stop_removed();
+    await VinylPages.DashboardPage.logout();
+  });
+
   test('Generate DRS statement via TA', async ({page}) => {
     await VinylPages.SignInPage.login(`${process.env.TA_USER1}`);
     await VinylPages.PhoneVerificationPage.enter_valid_otp();
@@ -51,7 +75,12 @@ test.describe("Vinyl Holder Management Tests", {tag: ['@search']}, async () => {
     await VinylPages.DashboardPage.go_to_holder_management();
     await expect(VinylPages.HolderManagementPage.holder_details).not.toBeVisible();
     await VinylPages.HolderManagementPage.search_for_holder(process.env.RO_USER1); 
-    await VinylPages.HolderManagementPage.validate_holder_details(process.env.RO2_USERNAME, process.env.RO_USER1, process.env.RO2_TIN, 'Individual');
-    await VinylPages.HolderManagementPage.generate_drs_statement_for_RO(process.env.RO2_USERNAME, `${process.env.ISSUER1} ${process.env.ISSUE1}`);
+    await VinylPages.HolderManagementPage.validate_holder_details_TA(process.env.RO2_USERNAME, process.env.RO_USER1, process.env.RO2_TIN, 'Individual');
+    var url = await VinylPages.HolderManagementPage.generate_drs_statement_for_RO(process.env.RO2_USERNAME, `${process.env.ISSUER1} ${process.env.ISSUE1}`, '2025-01-24', '2025-01-24');
+    await FileMethods.download_pdf_file(url);
+    var response_text = await FileMethods.parse_pdf();
+    var expected_text  = await FileMethods.generate_expected_text();
+    await expect(response_text).toEqual(expected_text);
+    await FileMethods.delete_downloaded_file();
   });
 });
