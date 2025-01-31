ece156ed8b831382c069dd097041bbdbd0cb496b
dilip-vinyl <dilip.kumar@vinylequity.com>
2025-01-31 16:38:08 +0530

    created different files

--- tests/holder_management/test_drs_statements.spec.ts
+++ tests/holder_management/test_drs_statements.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 import { vinylPages } from '../../PageObjects/vinylPages'
 import { fileMethods } from '../../support/fileMethods';
 
-test.describe("Vinyl Holder Management Tests", {tag: ['@dev_sanity', '@regression']}, async () => {
+test.describe("Vinyl Holder Management DRS statement tests", {tag: ['@dev_sanity', '@regression']}, async () => {
   let VinylPages: vinylPages;
   let FileMethods: fileMethods;
 
@@ -15,57 +15,6 @@ test.describe("Vinyl Holder Management Tests", {tag: ['@dev_sanity', '@regressio
   
   test.afterEach(async ({page}) => {
     await page.close();
-  });
-
-  test('Search for RO user via TA', { tag: '@search' }, async ({ page }) => {
-    var search_details = [process.env.RO_USER1, process.env.RO2_USERNAME, process.env.RO2_TIN, process.env.RO2_ACCOUNT_NUM];
-    await VinylPages.SignInPage.login(`${process.env.TA_USER1}`);
-    await VinylPages.PhoneVerificationPage.enter_valid_otp();
-    await page.waitForURL(`${process.env.HOST}dashboard`);
-    await VinylPages.DashboardPage.go_to_holder_management();
-    for(var i = 0; i < 4; i++){
-      await expect(VinylPages.HolderManagementPage.holder_details).not.toBeVisible();
-      await VinylPages.HolderManagementPage.search_for_holder(search_details[i]); 
-      await VinylPages.HolderManagementPage.validate_holder_details_TA(process.env.RO2_USERNAME, process.env.RO_USER1, process.env.RO2_TIN, 'Individual', process.env.RO2_ADDRESS);
-      await page.reload();
-    }
-    await VinylPages.DashboardPage.logout();
-  });
-  
-  test('Search for RO user via IA', { tag: '@search' }, async ({ page }) => {
-    var search_details = [process.env.RO_USER1, process.env.RO2_USERNAME, process.env.RO2_ACCOUNT_NUM];
-    await VinylPages.SignInPage.login(`${process.env.IA_USER1}`);
-    await VinylPages.PhoneVerificationPage.enter_valid_otp();
-    await page.waitForURL(`${process.env.HOST}dashboard`);
-    await VinylPages.DashboardPage.ia_holder.click();
-    for(var i = 0; i < 3; i++){
-      await expect(VinylPages.HolderManagementPage.holder_details).not.toBeVisible();
-      await VinylPages.HolderManagementPage.search_for_holder(search_details[i]); 
-      await VinylPages.HolderManagementPage.validate_holder_details_IA(process.env.RO2_USERNAME, process.env.RO_USER1, 'Individual', process.env.RO2_ADDRESS);
-      await page.reload();
-    }
-    await VinylPages.DashboardPage.logout();
-  });
-
-  test('Add and remove holder and position stop to a RO via TA', async ({ page }) => {
-    await VinylPages.SignInPage.login(`${process.env.TA_USER1}`);
-    await VinylPages.PhoneVerificationPage.enter_valid_otp();
-    await page.waitForURL(`${process.env.HOST}dashboard`);
-    await VinylPages.DashboardPage.go_to_holder_management();
-    await expect(VinylPages.HolderManagementPage.holder_details).not.toBeVisible();
-    await VinylPages.HolderManagementPage.search_for_holder(process.env.RO_USER1); 
-    await VinylPages.HolderManagementPage.validate_holder_details_TA(process.env.RO2_USERNAME, process.env.RO_USER1, process.env.RO2_TIN, 'Individual', process.env.RO2_ADDRESS);
-    //  Test Holder Stop
-    await VinylPages.HolderManagementPage.add_holder_stop();
-    await VinylPages.HolderManagementPage.validate_holder_stop_added();
-    await VinylPages.HolderManagementPage.remove_stop();
-    await VinylPages.HolderManagementPage.validate_holder_stop_removed();
-    // Test Position Stop
-    await VinylPages.HolderManagementPage.add_position_stop();
-    await VinylPages.HolderManagementPage.validate_position_stop_added();
-    await VinylPages.HolderManagementPage.remove_stop();
-    await VinylPages.HolderManagementPage.validate_position_stop_removed();
-    // await VinylPages.DashboardPage.logout();
   });
 
   test('Generate DRS statement via TA', async ({page}) => {
